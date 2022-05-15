b4c31e5b57f9dcabb045679abdc22137175c708a
DmitriyKaplan28 <dsk007@mail.ru>
2022-05-15 21:51:02 +0300

    HW7 without CSS

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import HW7 from "../../h7/HW7";
+
 
 
 function Junior() {
     return (
         <div>
-            Скоро здесь что-то будет, оставайтесь с нами
+            <HW7 />
 
         </div>
     )
