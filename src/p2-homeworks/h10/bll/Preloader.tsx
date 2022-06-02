b40b70c6a005020ca5a68223de45ef3c34a94014
DmitriyKaplan28 <dsk007@mail.ru>
2022-06-02 18:12:43 +0300

    HW10

---
+++ src/p2-homeworks/h10/bll/Preloader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => {
+    return (
+        <div>
+            <img src='https://cdn.dribbble.com/users/63485/screenshots/1309731/infinite-gif-preloader.gif' alt={'preloader'} style={{width: '80px'}}/>
+        </div>
+    );
+};
+
+export default Preloader;
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,33 @@
-const initState = {
-
+export type InitStateType = {
+    isLoading: boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type LoadingAT = {
+    type:'CHANGE-LOADING-STATUS'
+    isLoading: boolean
+}
+
+const initState = {
+    isLoading: false
+}
+
+const CHANGE_LOADING_STATUS = 'CHANGE-LOADING-STATUS'
+
+type ActionType = LoadingAT //понимаю, что в случае с одним экшеном это лишнее, сделал на случай если нужно будет добавить другие
+
+export const loadingReducer = (state:InitStateType = initState, action: ActionType): InitStateType => {
     switch (action.type) {
-        case '': {
-            return state
+        case CHANGE_LOADING_STATUS: {
+            return {...state, isLoading: action.isLoading}
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isLoading: boolean): LoadingAT => {
+    return {
+        type: CHANGE_LOADING_STATUS,
+        isLoading
+    } as const
+}
