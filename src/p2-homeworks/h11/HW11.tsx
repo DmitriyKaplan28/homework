ae4cc965a318e1f3b1a66c0fa8c935873dd55e8e
DmitriyKaplan28 <dsk007@mail.ru>
2022-06-16 22:51:10 +0300

    trying to fix HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -2,14 +2,25 @@ import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 import AlternativeSuperRange from "./AlternativeSuperRange";
+import AlternativeSuperDoubleRange from "./AlternativeSuperDoubleRange";
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
-    const onChangeRange = (value: number) => {
-      setValue1(value)
+    /*const onChangeRange = (value: number) => {
+        setValue1(value)
+    }*/
+    const onChangeRange = (event: Event, newValue: number) => {
+        setValue1(newValue);
     }
+
+
+    const [value, setValue] = useState<number[]>([value1, value2]);
+
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        setValue(newValue as number[]);
+    };
 
     return (
         <div>
@@ -19,32 +30,34 @@ function HW11() {
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange onChangeRange={onChangeRange}
+                <SuperRange value={value1}
+                            onChangeRange={onChangeRange}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
                 <span>{value1}</span>
-                <SuperDoubleRange
-                    min={value1}
-                    max={value2}
-                    onChange={({ min, max }: { min: number; max: number }) =>
-                        console.log(`min = ${min}, max = ${max}`)
-                    }
+                <SuperDoubleRange value={value}
+                                  handleChange={handleChange}
+                    //valuetext={valuetext}
                 />
                 <span>{value2}</span>
             </div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
-            <AlternativeSuperRange
+            {/*<AlternativeSuperRange
                 min={value1}
                 onChange={({ min}: { min: number }) =>
                     console.log(`min = ${min}`)
                 }
-            />
-            {/*<AlternativeSuperDoubleRange/>*/}
+            />*/}
+            <AlternativeSuperDoubleRange min={value1}
+                                         max={value2}
+                                         onChange={({min, max}: { min: number; max: number }) =>
+                                             console.log(`min = ${min}, max = ${max}`)
+                                         }/>
             <hr/>
         </div>
     )
--- src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
+++ src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
 import s from './SuperRange.module.css'
+import {Box, Slider} from "@mui/material";
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
@@ -7,7 +8,8 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
 type SuperRangePropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeRange?: (value: number) => void
+    onChangeRange?: (event: Event, newValue: number ) => void
+    value: number
 };
 
 const SuperRange: React.FC<SuperRangePropsType> = (
@@ -15,20 +17,32 @@ const SuperRange: React.FC<SuperRangePropsType> = (
         type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
         onChange, onChangeRange,
         className,
-
+        value,
         ...restProps// все остальные пропсы попадут в объект restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange && onChange(e) // сохраняем старую функциональность
+        //onChange && onChange(e) // сохраняем старую функциональность
 
-        onChangeRange && onChangeRange(+e.currentTarget.value)
+        onChangeRange
     }
 
-    const finalRangeClassName = `${s.range} ${className ? className : ''}`
+    /*const finalRangeClassName = `${s.range} ${className ? className : ''}`*/
+    function valuetext(value: number) {
+        return `${value}°C`;
+    }
 
     return (
-        <>
+
+        <Box sx={{width: 300}}>
+            <Slider getAriaLabel={() => 'Temperature range'}
+                    value={value}
+                    onChange={onChangeCallback}
+                    valueLabelDisplay="auto"
+                    getAriaValueText={valuetext}
+            />
+        </Box>
+        /*<>
             <input
                 type={'range'}
                 onChange={onChangeCallback}
@@ -36,7 +50,7 @@ const SuperRange: React.FC<SuperRangePropsType> = (
 
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
             />
-        </>
+        </>*/
     )
 }
 
